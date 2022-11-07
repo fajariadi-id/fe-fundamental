abab6ffaa562271afa3992f562741fdbcfbdef29
fajariadi-id <fajariadi.id@gmail.com>
2022-11-07 17:30:16 +0700

    progress

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import "../public/assets/css/styles.css";
+
+const sum = (a, b) => {
+  return a + b;
+};
+
+console.log(sum(12, 3));
+
+// alert("HAHAHAHAH");
---
+++ webpack.config.js
@@ -0,0 +1,57 @@
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path"); // untuk keperluan generate file kedalam suatu folder
+
+module.exports = {
+  mode: "development",
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "src"),
+    },
+    compress: true,
+    port: 3300,
+    liveReload: true,
+  },
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "./public/assets/js"),
+    filename: "main.[contenthash].js",
+    assetModuleFilename: "images/[hash][ext]",
+    clean: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/html/template.html",
+    }),
+    new MiniCssExtractPlugin({
+      filename: "main.[contenthash].css",
+    }),
+  ],
+  devtool: false,
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader"], // baca dari kanan
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+      {
+        test: /\.html$/i,
+        loader: "html-loader",
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+};
