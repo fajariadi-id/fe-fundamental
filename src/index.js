fea2c17ca84b706038e98775168d7c44e2bbb7f9
fajariadi-id <fajariadi.id@gmail.com>
2022-11-21 16:04:55 +0700

    progress modal

--- src/index.js
+++ src/index.js
@@ -31,23 +31,41 @@ new marquee(document.getElementById("coming-soon-3"), {
 // !:: -> search -> https://api.themoviedb.org/3/search/movie?api_key=8474014efb37e489ad1a5299e6ae7a88&query=${}
 // !:: -> img_path -> https://image.tmdb.org/t/p/original${}
 // !:: -> videos -> https://api.themoviedb.org/3/movie/663712/videos?api_key=8474014efb37e489ad1a5299e6ae7a88&language=en-US
-
-const API_URL =
-  "https://api.themoviedb.org/3/movie/now_playing?api_key=8474014efb37e489ad1a5299e6ae7a88&language=en-US&page=1";
+// !:: -> detail -> https://api.themoviedb.org/3/movie/663712?api_key=8474014efb37e489ad1a5299e6ae7a88&language=en-US
+// !:: -> credits -> https://api.themoviedb.org/3/movie/663712/credits?api_key=8474014efb37e489ad1a5299e6ae7a88&language=en-US
+const API_URL1 =
+  "https://api.themoviedb.org/3/movie/663712?api_key=8474014efb37e489ad1a5299e6ae7a88&language=en-US";
+const API_URL2 =
+  "https://api.themoviedb.org/3/movie/663712/credits?api_key=8474014efb37e489ad1a5299e6ae7a88&language=en-US";
 const IMG_PATH =
   "https://image.tmdb.org/t/p/original/oFAukXiMPrwLpbulGmB5suEZlrm.jpg";
 const SEARCH_API =
   "https://api.themoviedb.org/3/search/movie?api_key=8474014efb37e489ad1a5299e6ae7a88&query=";
 
-async function getMovies(url) {
-  const res = await fetch(url);
+async function getMovies(url1, url2) {
+  const res = await fetch(url1);
   const data = await res.json();
 
+  const results = await fetch(url2);
+  const credits = await results.json();
+
   console.log("data", data);
+  console.log("credits", credits);
 }
 
-getMovies(API_URL);
+getMovies(API_URL1, API_URL2);
 
+const body = document.querySelector("body");
 const btnMoreInfo = document.querySelector(".more-info");
+const modal = document.querySelector("#modal-detail");
+const closeModal = document.querySelector("#close-modal");
 
-btnMoreInfo.addEventListener("click", (e) => console.log("moreroemoremor"));
+btnMoreInfo.addEventListener("click", (e) => {
+  modal.classList.remove("hidden");
+  body.classList.add("overflow-hidden");
+});
+
+closeModal.addEventListener("click", () => {
+  modal.classList.add("hidden");
+  body.classList.remove("overflow-hidden");
+});
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,23 @@
 
 module.exports = {
   content: ["./src/template.html"],
+  corePlugins: {
+    container: false,
+  },
   theme: {
     extend: {
       fontFamily: {
         roboto: ["Roboto", "Arial"],
         poppins: ["Poppins", "Arial"],
+      },
+      colors: {
+        transparent: "transparent",
+        white: "#f6f6f4",
+        black: "#111",
+        red: "#B22727",
+        orange: "#EE5007",
+        yellow: "#F8CB2E",
+        blue: "#006E7F",
       },
     },
   },
