416375cc120a14511ccd31e2c354455c81465ea7
fajar <fajar@yukom.id>
2022-12-07 10:58:38 +0700

    update: banner dynamic

--- src/index.js
+++ src/index.js
@@ -37,23 +37,13 @@ const API_URL1 =
   "https://api.themoviedb.org/3/movie/663712?api_key=8474014efb37e489ad1a5299e6ae7a88&language=en-US";
 const API_URL2 =
   "https://api.themoviedb.org/3/movie/663712/credits?api_key=8474014efb37e489ad1a5299e6ae7a88&language=en-US";
-const IMG_PATH =
-  "https://image.tmdb.org/t/p/original/oFAukXiMPrwLpbulGmB5suEZlrm.jpg";
+const IMG_PATH = "https://image.tmdb.org/t/p/original";
 const SEARCH_API =
   "https://api.themoviedb.org/3/search/movie?api_key=8474014efb37e489ad1a5299e6ae7a88&query=";
 
-async function getMovies(url1, url2) {
-  const res = await fetch(url1);
-  const data = await res.json();
-
-  const results = await fetch(url2);
-  const credits = await results.json();
-
-  console.log("data", data);
-  console.log("credits", credits);
-}
-
-getMovies(API_URL1, API_URL2);
+const NOW_PLAYING = `https://api.themoviedb.org/3/movie/now_playing?api_key=8474014efb37e489ad1a5299e6ae7a88&language=en-US&page=${Math.ceil(
+  Math.random() * 10
+)}`;
 
 const body = document.querySelector("body");
 // const btnMoreInfo = document.querySelector(".more-info");
@@ -66,28 +56,19 @@ const banner = document.querySelector("#banner");
 // 20 -> selisih height 100px - 80px
 const navHeight = fixedNav.clientHeight - 20;
 
-// btnMoreInfo.addEventListener("click", (e) => {
-//   modal.classList.remove("hidden");
-//   body.classList.add("overflow-hidden");
-// });
-
 closeModal.addEventListener("click", () => {
   modal.classList.add("hidden");
   body.classList.remove("overflow-hidden");
 });
 
 window.addEventListener("DOMContentLoaded", (event) => {
-  showBanner();
+  getBanner();
 });
-// window.addEventListener("scroll", () => console.log("scrolll", window.scrollY));
-// NAV EVENT
 window.addEventListener("scroll", () => {
   const scrollHeight = window.scrollY;
   navTransition(scrollHeight);
 });
 
-// *=======  FUNCTION =======
-// NAV FUNC
 function navTransition(scroll) {
   if (scroll > navHeight) {
     fixedNav.classList.add("bg-black");
@@ -98,17 +79,18 @@ function navTransition(scroll) {
   }
 }
 
-const showBanner = () => {
+const showBanner = (data) => {
+  const { backdrop_path, title, original_title, overview } = data;
   const headerArticle = document.createElement("article");
   headerArticle.classList.add(
     "h-screen",
     "md:h-[80vh]",
-    "bg-[url('https://source.unsplash.com/random/600x600')]",
     "bg-no-repeat",
     "bg-center",
     "bg-cover",
     "relative"
   );
+  headerArticle.style.backgroundImage = `url('${IMG_PATH + backdrop_path}')`;
 
   const headerOverlay = document.createElement("div");
   headerOverlay.classList.add(
@@ -130,17 +112,15 @@ const showBanner = () => {
     "text-[36px]",
     "md:text-[48px]",
     "font-bold",
-    "font-poppins"
+    "font-poppins",
+    "leading-none"
   );
-  bannerTitle.innerText = `The Crown`;
+  bannerTitle.innerText = title ? title : original_title;
 
   const bannerDescription = document.createElement("p");
   bannerDescription.classList.add("pt-[20px]", "pb-[30px]");
-  bannerDescription.innerText = `The gripping, decades-spanning inside story of Her Majesty Queen
-                                Elizabeth II and the Prime Ministers who shaped Britain's post-war
-                                destiny. The Crown tells the inside story of two of the most
-                                famous addresses in the world – Buckingham Palace and 10 Downing
-                                Street – and...`;
+  bannerDescription.innerText =
+    overview.length <= 200 ? overview : overview.substr(0, 200) + "...";
 
   const btnMoreInfo = document.createElement("button");
   btnMoreInfo.classList.add(
@@ -158,7 +138,10 @@ const showBanner = () => {
     "rounded-[5px]"
   );
   btnMoreInfo.innerHTML = `<span class="font-bold">More info</span>`;
-  btnMoreInfo.addEventListener("click", () => console.log("addadadadadada"));
+  btnMoreInfo.addEventListener("click", () => {
+    modal.classList.remove("hidden");
+    body.classList.add("overflow-hidden");
+  });
 
   headerContent.appendChild(bannerTitle);
   headerContent.appendChild(bannerDescription);
@@ -170,4 +153,18 @@ const showBanner = () => {
   headerArticle.appendChild(headerContainer);
 
   banner.appendChild(headerArticle);
+};
+
+const getBanner = async () => {
+  try {
+    // ::
+    const res = await fetch(NOW_PLAYING);
+    const data = await res.json();
+
+    const bannerData =
+      data.results[Math.round(Math.random() * data.results.length)];
+    showBanner(bannerData);
+  } catch (error) {
+    console.error("fetch -> getBanner", error);
+  }
 };
