70962eed126103139bc52bcd28f32d628a2f3ca6
fajar <fajar@yukom.id>
2022-12-07 15:32:33 +0700

    update: coming soon movies

--- src/index.js
+++ src/index.js
@@ -1,28 +1,6 @@
 import marquee from "vanilla-marquee";
 import "./styles.css";
 
-new marquee(document.getElementById("coming-soon-1"), {
-  duplicated: true,
-  gap: 0,
-  speed: 50,
-  startVisible: true,
-});
-
-new marquee(document.getElementById("coming-soon-2"), {
-  duplicated: true,
-  gap: 0,
-  speed: 50,
-  startVisible: true,
-  direction: "right",
-});
-
-new marquee(document.getElementById("coming-soon-3"), {
-  duplicated: true,
-  gap: 0,
-  speed: 100,
-  startVisible: true,
-});
-
 // !======= API =======*
 // !:: -> now playing -> https://api.themoviedb.org/3/movie/now_playing?api_key=8474014efb37e489ad1a5299e6ae7a88&language=en-US&page=1
 // !:: -> upcoming -> https://api.themoviedb.org/3/movie/upcoming?api_key=8474014efb37e489ad1a5299e6ae7a88&language=en-US&page=1
@@ -45,12 +23,20 @@ const NOW_PLAYING = `https://api.themoviedb.org/3/movie/now_playing?api_key=8474
   Math.random() * 10
 )}`;
 
+const COMING_SOON_1 =
+  "https://api.themoviedb.org/3/movie/upcoming?api_key=8474014efb37e489ad1a5299e6ae7a88&language=en-US&page=1";
+const COMING_SOON_2 =
+  "https://api.themoviedb.org/3/movie/upcoming?api_key=8474014efb37e489ad1a5299e6ae7a88&language=en-US&page=2";
+const COMING_SOON_3 =
+  "https://api.themoviedb.org/3/movie/upcoming?api_key=8474014efb37e489ad1a5299e6ae7a88&language=en-US&page=3";
+
 const body = document.querySelector("body");
 // const btnMoreInfo = document.querySelector(".more-info");
 const modal = document.querySelector("#modal-detail");
 const closeModal = document.querySelector("#close-modal");
 const fixedNav = document.querySelector(".fixed-nav");
 const banner = document.querySelector("#banner");
+const comingSoon = document.querySelector("#coming-soon");
 
 // NAV HEIGHT
 // 20 -> selisih height 100px - 80px
@@ -63,6 +49,7 @@ closeModal.addEventListener("click", () => {
 
 window.addEventListener("DOMContentLoaded", (event) => {
   getBanner();
+  getComingSoon();
 });
 window.addEventListener("scroll", () => {
   const scrollHeight = window.scrollY;
@@ -155,15 +142,165 @@ const showBanner = (data) => {
   banner.appendChild(headerArticle);
 };
 
+const generateRow = (container, row) => {
+  row.forEach((movie, index) => {
+    const poster = document.createElement("div");
+    poster.classList.add(
+      "w-[200px]",
+      "h-[100px]",
+      "md:w-[250px]",
+      "md:h-[130px]",
+      "py-[10px]",
+      "px-[25px]",
+      "rounded-[15px]",
+      "bg-red",
+      "relative",
+      "bg-gradient-to-b",
+      "from-red",
+      "to-orange"
+    );
+
+    const img = document.createElement("img");
+    img.classList.add(
+      "block",
+      "w-full",
+      "h-full",
+      "object-cover",
+      "rounded-[10px]"
+    );
+    img.alt = movie.title;
+    img.src = IMG_PATH + movie.backdrop_path;
+
+    const leftDots = document.createElement("div");
+    leftDots.classList.add(
+      "absolute",
+      "left-[-2.5px]",
+      "top-[7.5px]",
+      "md:left-[-5px]",
+      "md:top-[10px]",
+      "z-[1]"
+    );
+    leftDots.innerHTML = `
+      <div class="w-[5px] h-[5px] md:w-[10px] md:h-[10px] bg-black rounded-full mt-[10px]"></div>
+      <div class="w-[5px] h-[5px] md:w-[10px] md:h-[10px] bg-black rounded-full mt-[10px]"></div>
+      <div class="w-[5px] h-[5px] md:w-[10px] md:h-[10px] bg-black rounded-full mt-[10px]"></div>
+      <div class="w-[5px] h-[5px] md:w-[10px] md:h-[10px] bg-black rounded-full mt-[10px]"></div>
+      <div class="w-[5px] h-[5px] md:w-[10px] md:h-[10px] bg-black rounded-full mt-[10px]"></div>
+    `;
+
+    const rightDots = document.createElement("div");
+    rightDots.classList.add(
+      "absolute",
+      "right-[-2.5px]",
+      "top-[7.5px]",
+      "md:right-[-5px]",
+      "md:top-[10px]",
+      "z-[1]"
+    );
+    rightDots.innerHTML = `
+      <div class="w-[5px] h-[5px] md:w-[10px] md:h-[10px] bg-black rounded-full mt-[10px]"></div>
+      <div class="w-[5px] h-[5px] md:w-[10px] md:h-[10px] bg-black rounded-full mt-[10px]"></div>
+      <div class="w-[5px] h-[5px] md:w-[10px] md:h-[10px] bg-black rounded-full mt-[10px]"></div>
+      <div class="w-[5px] h-[5px] md:w-[10px] md:h-[10px] bg-black rounded-full mt-[10px]"></div>
+      <div class="w-[5px] h-[5px] md:w-[10px] md:h-[10px] bg-black rounded-full mt-[10px]"></div>
+      <div class="w-[5px] h-[5px] md:w-[10px] md:h-[10px] bg-black rounded-full mt-[10px]"></div>
+      `;
+
+    poster.appendChild(img);
+    poster.appendChild(rightDots);
+    if (index === 0) poster.appendChild(leftDots);
+
+    container.appendChild(poster);
+  });
+};
+
+const showComingSoon = (data) => {
+  const { row_1, row_2, row_3 } = data;
+
+  const comingSoonRow1 = document.createElement("article");
+  comingSoonRow1.classList.add("overflow-hidden");
+
+  const comingSoonRow2 = document.createElement("article");
+  comingSoonRow2.classList.add("overflow-hidden", "mt-[10px]");
+
+  const comingSoonRow3 = document.createElement("article");
+  comingSoonRow3.classList.add("overflow-hidden", "mt-[10px]");
+
+  const posterContainerRow1 = document.createElement("div");
+  posterContainerRow1.classList.add("flex");
+
+  const posterContainerRow2 = document.createElement("div");
+  posterContainerRow2.classList.add("flex");
+
+  const posterContainerRow3 = document.createElement("div");
+  posterContainerRow3.classList.add("flex");
+
+  generateRow(posterContainerRow1, row_1);
+  generateRow(posterContainerRow2, row_2);
+  generateRow(posterContainerRow3, row_3);
+
+  comingSoonRow1.appendChild(posterContainerRow1);
+  comingSoonRow2.appendChild(posterContainerRow2);
+  comingSoonRow3.appendChild(posterContainerRow3);
+
+  comingSoon.appendChild(comingSoonRow1);
+  comingSoon.appendChild(comingSoonRow2);
+  comingSoon.appendChild(comingSoonRow3);
+
+  new marquee(comingSoonRow1, {
+    duplicated: true,
+    gap: 0,
+    speed: 50,
+    startVisible: true,
+  });
+
+  new marquee(comingSoonRow2, {
+    duplicated: true,
+    gap: 0,
+    speed: 50,
+    startVisible: true,
+    direction: "right",
+  });
+
+  new marquee(comingSoonRow3, {
+    duplicated: true,
+    gap: 0,
+    speed: 100,
+    startVisible: true,
+  });
+};
+
 const getBanner = async () => {
   try {
-    // ::
     const res = await fetch(NOW_PLAYING);
     const data = await res.json();
 
     const bannerData =
       data.results[Math.round(Math.random() * data.results.length)];
     showBanner(bannerData);
+  } catch (error) {
+    console.error("fetch -> getBanner", error);
+  }
+};
+
+const getComingSoon = async () => {
+  try {
+    const res1 = await fetch(COMING_SOON_1);
+    const data1 = await res1.json();
+
+    const res2 = await fetch(COMING_SOON_2);
+    const data2 = await res2.json();
+
+    const res3 = await fetch(COMING_SOON_3);
+    const data3 = await res3.json();
+
+    const data = {
+      row_1: data1.results,
+      row_2: data2.results,
+      row_3: data3.results,
+    };
+
+    showComingSoon(data);
   } catch (error) {
     console.error("fetch -> getBanner", error);
   }
